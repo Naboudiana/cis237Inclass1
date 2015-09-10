8320a5ec81ca7a56d2d06f5e40aaa6ed54c3c453
Seynabou Diagne <sddiagne21@gmail.com>
2015-09-10 15:56:38 -0400

    Add person class. Add some lines to the main.

---
+++ cis237Inclass1/Person.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Person
+    {
+        string firstName;
+        string lastName;
+        int Age;
+
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+
+        }
+
+        public int age
+        {
+            get
+            {
+                return this.Age;
+            }
+            set
+            {
+                this.Age = value;
+            }
+
+        }
+
+        public Person()
+        {
+            
+        }
+
+        public Person(string firstName, string lastName, int Age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.Age = Age;
+        }
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(this.firstName + ' ' + this.lastName);
+        }
+
+        public void PrintAge()
+        {
+            Console.WriteLine(this.Age);
+        }
+
+        public void Identity()
+        {
+            Console.WriteLine("Name= " + this.firstName + ' ' + this.lastName);
+            Console.WriteLine("Age= " + this.Age);
+        }
+
+
+      
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,24 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person("Seynabou", "Diagne", 24);
+
+            Person person2 = new Person("Abou", "Mbodji", 27);
+
+            person1.PrintFullName();
+            person1.PrintAge();
+            Console.WriteLine();
+            person2.PrintFullName();
+            person2.PrintAge();
+
+            Console.WriteLine("\n\n");
+
+            person1.Identity();
+            Console.WriteLine("\n");
+            person2.Identity();
+
+
+            Console.ReadLine();
         }
     }
 }
